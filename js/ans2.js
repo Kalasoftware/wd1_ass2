059f3f1992f40021cd075022228a15cfe1c9c20b
Kalasoftware <newsoftwarewala@gmail.com>
2024-03-04 23:58:32 +0530

    made the changes in js of answer 2

--- js/ans2.js
+++ js/ans2.js
@@ -1,5 +1,13 @@
 function val()
 {
-    var allin = document.querySelectorAll("input");
-    alert(allin.length);
+    var hname = document.forms["hotelreg"]["hname"];
+    if(hname.value.trim() == "")
+    {
+        alert("bro kuch to dal ");
+
+    }
+    else 
+    {
+        console.log("sahi hai jane de ");
+    }
 }
